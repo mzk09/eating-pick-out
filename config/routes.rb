fa37e52001ccede0a2ff3eda72f3fc97a2d8dd91
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-11-20 12:51:55 +0000

    [add]public/customer show edit,admin/customer index show edit機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,42 @@
 Rails.application.routes.draw do
+  
   namespace :admin do
+    #get 'top' => 'homes#top', as: 'top'
+    #get 'search' => 'homes#search', as: 'search'
+    resources :customers, only: [:index, :show, :edit, :update]
     resources:restaurants,only:[:index,:new,:create,:show,:edit,:update,:destroy]
+    #resources :genres, only: [:index, :create, :edit, :update]
+    #resources :orders, only: [:index, :show, :update] do
+      #resources :order_details, only: [:update]
+    #end
   end
-  root 'public/homes#top'
+  
+  scope module: :public do
+    root 'homes#top'
+    
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+    # customers/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている。
+    get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customers/information' => 'customers#update', as: 'update_information'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    put 'customers/information' => 'customers#update'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+    #delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all_cart_items'
+    
+    
+    #resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+    #resources :items, only: [:index, :show] do
+      #resources :cart_items, only: [:create, :update, :destroy]
+    #end
+    #resources :cart_items, only: [:index]
+    #resources :orders, only: [:new, :index, :create, :show]
+    
+  end
+
 
   namespace :public do
   end
-  
+
   devise_for :customers,controllers:{
     registrations:"public/registrations",
     sessions:'public/sessions'
