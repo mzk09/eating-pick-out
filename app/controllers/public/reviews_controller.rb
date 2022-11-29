e7a5988540a8885d3f8e0c2a2c33f117bc2d9222
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-11-29 09:56:23 +0000

    review.comment機能実装（保存・削除まで）

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,16 +1,23 @@
 class Public::ReviewsController < ApplicationController
+  before_action :authenticate_customer!, only:[:create,:destroy
+]
 
   def create
     restaurant = Restaurant.find(params[:restaurant_id])
     review = current_customer.reviews.new(review_params)
     review.restaurant_id = restaurant.id
-    review.save
+    review.save!
     redirect_to restaurant_path(restaurant)
+  end
+
+  def destroy
+    Review.find(params[:id]).destroy
+    redirect_to restaurant_path(params[:restaurant_id])
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:comment,:rate)
+    params.require(:review).permit(:comment,:rate,:customer_id,:restaurant_id)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,14 @@
 class Review < ApplicationRecord
   belongs_to :customer
   belongs_to :restaurant
-  
-  validates :rate, numericality: {
-    less_than_or_equal_to: 5,
-    greater_than_or_equal_to: 1}, presence: true
-    
-    
-    
+
+    validates:comment,presence:true
+
+  #↓評価機能を付けるまではコメントアウト
+  #validates :rate, numericality: {
+  # less_than_or_equal_to: 5,
+   # greater_than_or_equal_to: 1}, presence: true
+
+
+
 end
