97d1be7a66eba51fab6869b7cd56f4b5c9e5f3a8
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2023-01-09 11:36:58 +0000

    migrate修正

--- db/migrate/20221117115942_create_restaurants.rb
+++ db/migrate/20221117115942_create_restaurants.rb
@@ -4,7 +4,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.1]
 
       t.string :name,   null: false, default: ""
       t.string :business_time,  null: false, default: ""
-      t.integer :price, null: false, default: ""
+      t.integer :price, null: false, default: 0
       t.string :telephone_number, null: false, default: ""
       t.string :address,  null: false, default: ""
       t.boolean :is_active, null: false, default: true
